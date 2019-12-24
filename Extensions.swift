70908764b2e2b0fe9d7cdab2f142cef1242b4898
Andrey Posnov <mailbox@aposnov.ru>
2019-12-25 01:39:33 +0300

    Update Extensions.swift

--- Extensions.swift
+++ Extensions.swift
@@ -1,1 +1,196 @@
+// versions of app
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+}
 
+// only unique in array
+extension Array where Element : Equatable {
+    var unique: [Element] {
+        var uniqueValues: [Element] = []
+        forEach { item in
+            if !uniqueValues.contains(item) {
+                uniqueValues += [item]
+            }
+        }
+        return uniqueValues
+    }
+}
+
+//string to date and remove white spaces
+extension String {
+    var toDate: Date {
+        return Date.Formatter.customDate.date(from: self)!
+    }
+    
+    func removeWhiteSpace() -> String {
+        return self.replacingOccurrences(of: " ", with: "")
+    }
+}
+
+extension Date {
+    struct Formatter {
+        static let customDate: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.dateFormat = "dd.MM.yy"
+            return formatter
+        }()
+    }
+}
+
+// different bolds in strings
+extension NSMutableAttributedString {
+    @discardableResult func bold(_ text: String) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Ubuntu-Medium", size: 20)
+        ]
+        let boldString = NSMutableAttributedString(string:text, attributes: attrs)
+        append(boldString)
+        
+        return self
+    }
+    
+    @discardableResult func normal(_ text: String) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Ubuntu", size: 20)
+        ]
+        let normal = NSMutableAttributedString(string:text, attributes: attrs)
+        append(normal)
+        
+        return self
+    }
+}
+
+// Sketch/zeppling/figma shadow to CALayer
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        masksToBounds = false
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
+
+// add padding to textfield
+extension UITextField {
+    enum PaddingSide {
+        case left(CGFloat)
+        case right(CGFloat)
+        case both(CGFloat)
+    }
+    
+    func addPadding(_ padding: PaddingSide) {
+        
+        self.leftViewMode = .always
+        self.layer.masksToBounds = true
+        
+        switch padding {
+            
+        case .left(let spacing):
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
+            self.leftView = paddingView
+            self.rightViewMode = .always
+            
+        case .right(let spacing):
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
+            self.rightView = paddingView
+            self.rightViewMode = .always
+            
+        case .both(let spacing):
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: spacing, height: self.frame.height))
+            // left
+            self.leftView = paddingView
+            self.leftViewMode = .always
+            // right
+            self.rightView = paddingView
+            self.rightViewMode = .always
+        }
+    }
+}
+
+// hide keyboard when tap arround
+extension UIViewController {
+    public func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer =     UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+// delete .0, of 5.0 it will be 5, if it 5.03 it will be 5.03. Убираем 0 после точки, если нет плавающего значения.
+extension Double {
+    var clean: String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
+extension Float {
+    var clean: String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
+
+// for parsing json api, encode for send model to server, decode for receive models. 
+// encodesorted will sort json keys
+// toString will convert model to string
+extension Encodable {
+    func toString() -> String {
+        guard let data = try? JSONEncoder().encode(self) else { return "" }
+        return String(data: data, encoding: .utf8) ?? ""
+    }
+    
+    func encode(with encoder: JSONEncoder = JSONEncoder()) throws -> Data {
+        return try encoder.encode(self)
+    }
+    
+    func encodeSorted(with encoder: JSONEncoder = JSONEncoder()) throws -> Data {
+        encoder.outputFormatting = .sortedKeys
+        return try encoder.encode(self)
+    }
+}
+
+extension Decodable {
+    static func decode(with decoder: JSONDecoder = JSONDecoder(), from data: Data) throws -> Self? {
+        do {
+            let newdata = try decoder.decode(Self.self, from: data)
+            return newdata
+        } catch {
+            print("decodable model error", error.localizedDescription)
+            return nil
+        }
+    }
+    static func decodeArray(with decoder: JSONDecoder = JSONDecoder(), from data: Data) throws -> [Self]{
+        do {
+            let newdata = try decoder.decode([Self].self, from: data)
+            return newdata
+        } catch {
+            print("decodable model error", error.localizedDescription)
+            return []
+        }
+    }
+}
